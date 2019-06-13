e60c37b3315b065d3a1c9666c96b92af8c1988d4
seldii <seldaguzel@gmail.com>
2019-06-13 17:29:12 +0200

    again

--- my-app/src/store/store.js
+++ my-app/src/store/store.js
@@ -6,7 +6,7 @@ import axios from "axios";
 Vue.use(Vuex);
 Vue.use(Vuetify);
 const config = {
-  "user-key": "1fed55d6e1bf7fa795601c5deac270ae",
+  "user-key": "",
   Accept: "application/json"
 };
 
@@ -26,7 +26,7 @@ const store = new Vuex.Store({
       const response = await axios.get(
         `https://maps.googleapis.com/maps/api/geocode/json?address=` +
           e +
-          `&key=AIzaSyBXjKG5EqxMNpeCQKD5EVe9sZiHPYVuxJ0`
+          `&key=`
       );
 
       lat = response.data.results[0].geometry.location.lat;
