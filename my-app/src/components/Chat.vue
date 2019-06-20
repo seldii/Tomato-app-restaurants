0c07a436a92bc55fa579f4b83b867867f6aadb48
seldii <seldaguzel@gmail.com>
2019-06-20 18:36:20 +0200

    profile styling

--- my-app/src/components/Chat.vue
+++ my-app/src/components/Chat.vue
@@ -1,21 +1,21 @@
 <template>
   <v-container class="chat-wrapper">
     <v-spacer></v-spacer>
-    <v-layout row>
+    <v-layout row wrap>
       <v-flex grow pa-1>
-        <v-avatar size="10em">
+        <v-avatar size="6em">
           <img :src="currentUser.photoURL">
         </v-avatar>
       </v-flex>
       <v-flex shrink>
         <div class="subheading">{{currentUser.displayName}}</div>
-        <div class="body-1">{{ currentUser.email }}</div>
+        <div class="caption">{{ currentUser.email }}</div>
         <v-btn @click.prevent="logout()" text outline color="indigo" left small>Log out</v-btn>
       </v-flex>
     </v-layout>
     <v-spacer></v-spacer>
-    <v-tabs dark color="transparent">
-      <v-tabs-slider color="yellow"></v-tabs-slider>
+    <!-- <v-tabs dark color="transparent">
+      <v-tabs-slider color=""></v-tabs-slider>
 
       <v-tab color="transparent">
         <v-card flat>
@@ -27,42 +27,95 @@
           <v-card-text>Chat</v-card-text>
         </v-card>
       </v-tab>
-    </v-tabs>
+    </v-tabs>-->
 
     <v-container fluid>
       <v-layout row align-baseline>
         <v-flex xs-11>
           <v-form class="chat-submit" @submit.prevent="addMessage">
             <v-text-field
+              append-outer-icon="mdi-send"
               type="text"
               class="new-message"
               v-model="message"
-              placeholder="Say something..."
+              label="Message"
+              @click:append-outer="addMessage"
+              clearable
             ></v-text-field>
           </v-form>
-        </v-flex>
-        <v-flex shrink>
-          <v-btn icon>
-            <v-icon color="orange darken-2" @click="addMessage">mail_outline</v-icon>
-          </v-btn>
         </v-flex>
       </v-layout>
     </v-container>
     <h3 class="text-xs-center">Recent Chats</h3>
     <v-spacer></v-spacer>
-    <v-timeline ref="chatList">
+    <v-container ref="chatList" fluid grid-list-sm>
+      <v-layout align-center justify-space-between wrap>
+        <v-flex xs12 v-for="message in messages" :key="message.username">
+          <v-layout row wrap>
+            <v-flex
+              xs5
+              offset-xs7
+              color="orange darken-2"
+              small
+              v-if="currentUser.displayName == message.username"
+            >
+              <v-badge color="purple" right overlap>
+                <template v-slot:badge>
+                  <v-icon small dark>message</v-icon>
+                </template>
+                <v-card color="orange lighten-4" full-width>
+                  <v-card-title>
+                    <span class="title font-weight-light caption">You</span>
+                  </v-card-title>
+                  <v-card-text class="headline font-weight-bold body-2">"{{ message.text }}"</v-card-text>
+                </v-card>
+              </v-badge>
+            </v-flex>
+            <v-flex xs5 small v-if="currentUser.displayName != message.username">
+              <v-badge color="purple" right overlap>
+                <template v-slot:badge>
+                  <v-icon small dark>message</v-icon>
+                </template>
+                <v-card color="blue-grey lighten-3">
+                  <v-card-title>
+                    <v-icon dark>account_circle</v-icon>
+                    <span cclass="title font-weight-light caption">{{ message.username }}</span>
+                  </v-card-title>
+                  <v-card-text class="headline font-weight-bold body-2">"{{ message.text }}"</v-card-text>
+                </v-card>
+              </v-badge>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <!-- <v-timeline ref="chatList">
       <v-timeline-item
         color="orange darken-2"
         small
         v-for="message in messages"
         :key="message.username"
       >
-        <div class="chat-message__body">
+        <div
+          v-if="currentUser.displayName == message.username"
+          style="background-color:blue;"
+          class="chat-message__body"
+        >
+          <span>{{ message.username }}</span>
+
+          <p class="text-xs-right">"{{ message.text }}"</p>
+        </div>
+        <div
+          v-if="currentUser.displayName != message.username"
+          style="background-color:red;"
+          class="chat-message__body"
+        >
           <p class="text-xs-right">{{ message.username }}</p>
           <p class="text-xs-right">"{{ message.text }}"</p>
         </div>
       </v-timeline-item>
-    </v-timeline>
+    </v-timeline>-->
   </v-container>
 </template>
 
@@ -105,7 +158,6 @@ export default {
           text: this.message
         });
         this.message = "";
-        console.log(this.$store.state.user.currentUser);
       }
     }
   },
